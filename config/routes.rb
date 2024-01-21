2cd7bdc22798b39970fb9d4d66e99f9e9b9e5198
root <root@localhost>
2024-01-21 17:50:14 +0000

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  
+  root "main#index"
+
+  match 'about', to: 'main#about', via: :get
+  # go to the main controller and look for the about method
+
+  get 'main/index'
+  get 'tasks/index'
+  get 'tasks/new'
+  get 'tasks/edit'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
